0e933b1617732298b93fabd2afe406d23b8983bb
Ryan Atkinson <mail@ryanatkn.com>
2023-09-22 10:11:05 -0600

    wip

--- src/lib/LibraryHeader.svelte
+++ src/lib/LibraryHeader.svelte
@@ -1,3 +1,4 @@
 <script lang="ts">
-	//  TODO BLOCK LibraryHeader is the thing that sits above the content page on the homepage of a repo that has only one doc page (like contextmenu)
+	//  TODO BLOCK LibraryHeader is the thing that sits above the content page
+	// on the homepage of a repo that has only one doc page (like contextmenu) and thus no library
 </script>
