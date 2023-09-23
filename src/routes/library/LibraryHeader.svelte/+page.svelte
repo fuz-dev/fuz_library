93fae8caf2d5ab32e7497ffceaa7a3b72307eddc
Ryan Atkinson <mail@ryanatkn.com>
2023-09-23 09:46:29 -0600

    wip

--- src/routes/library/LibraryHeader.svelte/+page.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-	import LibraryHeader from '$lib/LibraryHeader.svelte';
-	import LibraryVocab from '$lib/LibraryVocab.svelte';
-	import type {LibraryItemData} from '$lib/library_item';
-
-	const item: LibraryItemData = {};
-</script>
-
-The <LibraryVocab name="LibraryHeader" /> - <LibraryHeader {item} />
