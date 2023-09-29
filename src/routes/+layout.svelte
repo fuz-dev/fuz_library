e62d5a08dba6d1da46ace64e9267d9e9ef662fe3
Ryan Atkinson <mail@ryanatkn.com>
2023-09-29 03:01:34 -0600

    wip

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import '@fuz.dev/fuz/style.css';
 	import '@fuz.dev/fuz/theme.css';
-	import '@fuz.dev/fuz_code/prism.css'; // TODO BLOCK maybe import from Code?
+	import '@fuz.dev/fuz_code/prism.css'; // TODO maybe import from Code?
 
 	import Themed from '@fuz.dev/fuz/Themed.svelte';
 </script>
