b97a020a0573ac3d2c7ee0eb04082e8bac123326
Ryan Atkinson <mail@ryanatkn.com>
2023-09-30 01:06:44 -0600

    wip

--- src/lib/fetch_packages.ts
+++ src/lib/fetch_packages.ts
@@ -7,17 +7,17 @@ import type {Package} from '$lib/package.js';
 
 /* eslint-disable no-await-in-loop */
 
-export const fetch_packages = async (hosts: Url[], log?: Logger): Promise<Package[]> => {
+export const fetch_packages = async (urls: Url[], log?: Logger): Promise<Package[]> => {
 	const packages: Package[] = [];
-	for (const host of hosts) {
-		const url = strip_end(host, '/') + '/.well-known/package.json';
-		log?.info('fetching', url);
-		const res = await fetch(url, {
+	for (const url of urls) {
+		const package_json_url = strip_end(url, '/') + '/.well-known/package.json';
+		log?.info('fetching', package_json_url);
+		const res = await fetch(package_json_url, {
 			headers: {'content-type': 'application/json', accept: 'application/json'},
 		});
 		const json = await res.json();
 		const package_json = PackageJson.parse(json);
-		packages.push({url: host, package_json});
+		packages.push({url, package_json});
 		// TODO delay?
 	}
 	return packages;
