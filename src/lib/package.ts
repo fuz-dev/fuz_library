0f6d9be638b258efc1c1e45da71146cdc9e3795f
Ryan Atkinson <mail@ryanatkn.com>
2023-09-30 16:08:55 -0600

    wip

--- src/lib/package.ts
+++ src/lib/package.ts
@@ -4,4 +4,10 @@ import type {Url} from '@grogarden/gro/paths.js';
 export interface Package {
 	url: Url;
 	package_json: PackageJson;
+	npm_url: string; // 'https://npmjs.com/package/@fuz.dev/fuz_library';
+	name: string; // '@fuz.dev/fuz_library';
+	repo_url: string; // 'https://github.com/fuz-dev/fuz_library';
+	org_url: string; // 'https://github.com/fuz-dev';
+	homepage_url: string; // 'https://www.fuz.dev/';
+	homepage_name: string; // 'fuz.dev';
 }
