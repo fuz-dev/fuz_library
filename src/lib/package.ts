15b1bda7778eae44bf276b0e4ba3f7da338a59c2
Ryan Atkinson <mail@ryanatkn.com>
2023-09-30 16:00:39 -0600

    wip

--- src/lib/package.ts
+++ src/lib/package.ts
@@ -1,7 +1,6 @@
 import type {PackageJson} from '@grogarden/gro/package_json.js';
 import type {Url} from '@grogarden/gro/paths.js';
 
-// TODO rename ? `Tome` was `LibraryItem`, maybe `Pkg` so it's consistent? then `pkgs` etc
 export interface Package {
 	url: Url;
 	package_json: PackageJson;
