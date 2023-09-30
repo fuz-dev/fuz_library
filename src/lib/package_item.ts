b8b7ea79fab0d8dbae18b9ef754d808851fcecd4
Ryan Atkinson <mail@ryanatkn.com>
2023-09-29 19:11:07 -0600

    wip

--- src/lib/package_item.ts
+++ src/lib/package_item.ts
@@ -1,7 +1,7 @@
 import type {PackageJson} from '@grogarden/gro/package_json.js';
 import type {Url} from '@grogarden/gro/paths.js';
 
-// TODO rename
+// TODO rename ? `Tome` was `LibraryItem`
 
 export interface PackageItem {
 	host: Url;
