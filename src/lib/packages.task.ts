378dd3b26b24d254601fc95f29bb745f3226ccc8
Ryan Atkinson <mail@ryanatkn.com>
2023-09-30 01:48:16 -0600

    fix packages task

--- src/lib/packages.task.ts
+++ src/lib/packages.task.ts
@@ -9,15 +9,7 @@ import {exists} from '@grogarden/gro/exists.js';
 import {fetch_packages} from '$lib/fetch_packages.js';
 import type {Package} from '$lib/package.js';
 
-// TODO refactor/rename
-export const default_package_urls = [
-	// TODO add more
-	// 'https://www.fuz.dev/',
-	'https://www.grogarden.org/',
-	'https://template.fuz.dev/',
-	'https://code.fuz.dev/',
-	// 'https://www.felt.dev/',
-];
+export const default_package_urls = ['https://www.grogarden.org/', 'https://template.fuz.dev/'];
 
 // TODO maybe support `--check` for CI
 export const Args = z
@@ -25,31 +17,38 @@ export const Args = z
 		_: z
 			.array(Url, {description: 'package host URLs, the paths containing .well-known'})
 			.default(default_package_urls),
+		outfile: z
+			.string({description: 'path to the generated packages.json file'})
+			.default('./src/lib/packages.json'),
 	})
 	.strict();
 export type Args = z.infer<typeof Args>;
 
+/**
+ * This is a task not a `.gen.` file because it makes network calls.
+ */
 export const task: Task<Args> = {
 	Args,
 	summary: 'download metadata for the given packages',
 	run: async ({args, log}) => {
-		const {_: package_urls} = args;
-
-		const root_package_json = await load_package_json();
+		const {_: package_urls, outfile} = args;
 
 		const fetched_packages = await fetch_packages(package_urls, log);
 
-		const packages: Package[] = [
-			{url: 'https://library.fuz.dev/', package_json: root_package_json},
-		].concat(fetched_packages);
+		const root_package_json = await load_package_json();
 
-		const out_path = './src/lib/packages.json';
-		await writeFile(out_path, await format_file(out_path, JSON.stringify(packages)));
+		const packages: Package[] = root_package_json.homepage
+			? [{url: root_package_json.homepage, package_json: root_package_json}].concat(
+					fetched_packages,
+			  )
+			: fetched_packages;
 
-		const types_out_path = out_path + '.d.ts';
-		if (!(await exists(types_out_path))) {
+		await writeFile(outfile, await format_file(outfile, JSON.stringify(packages)));
+
+		const types_outfile = outfile + '.d.ts';
+		if (!(await exists(types_outfile))) {
 			await writeFile(
-				types_out_path,
+				types_outfile,
 				`declare module '$lib/packages.json' {
 	import type {Package} from '@fuz.dev/fuz_library/package.js';
 	const data: Package[];
