b0ed97f6fc62ec0b9a37aff608d07157efcd064a
Ryan Atkinson <mail@ryanatkn.com>
2023-09-30 16:30:01 -0600

    wip

--- src/lib/PackageSummary.svelte
+++ src/lib/PackageSummary.svelte
@@ -1,46 +1,9 @@
 <script lang="ts">
-	import {strip_start, strip_end} from '@grogarden/util/string.js';
-
 	import type {Package} from '$lib/package.js';
 
 	export let pkg: Package; // TODO normalized version with cached primitives?
 
-	$: ({package_json} = pkg);
-
-	$: ({name} = package_json);
-
-	// TODO think through with other presentations - Details, Summary, Card
-
-	// TODO hacky
-	const parse_repo = (r: string | null | undefined) => {
-		if (!r) return null;
-		return strip_start(strip_end(r, '.git'), 'git+');
-	};
-
-	$: repo = parse_repo(
-		package_json.repository
-			? typeof package_json.repository === 'string'
-				? package_json.repository
-				: package_json.repository.url
-			: null,
-	);
-
-	const format_host = (homepage: string): string => strip_start(new URL(homepage).host, 'www.');
-
-	// TODO for detail view
-	// $: license_url = license && repository ? repository + '/blob/main/LICENSE' : null;
-
-	$: published =
-		!package_json.private && !!package_json.exports && package_json.version !== '0.0.1';
-
-	$: npm_url = published
-		? 'https://www.npmjs.com/package/' + encodeURIComponent(package_json.name)
-		: null;
-
-	$: changelog_url = published && repo ? repo + '/blob/main/CHANGELOG.md' : null;
-
-	// TODO proper parsing
-	$: repo_name = name[0] === '@' ? name.split('/')[1] : name;
+	$: ({package_json, npm_url} = pkg);
 </script>
 
 <div class="package_summary">
--- src/lib/package.ts
+++ src/lib/package.ts
@@ -1,13 +1,64 @@
 import type {PackageJson} from '@grogarden/gro/package_json.js';
 import type {Url} from '@grogarden/gro/paths.js';
+import {strip_start, strip_end} from '@grogarden/util/string.js';
 
 export interface Package {
 	url: Url;
 	package_json: PackageJson;
-	npm_url: string; // 'https://npmjs.com/package/@fuz.dev/fuz_library';
 	name: string; // '@fuz.dev/fuz_library';
+	npm_url: string; // 'https://npmjs.com/package/@fuz.dev/fuz_library';
 	repo_url: string; // 'https://github.com/fuz-dev/fuz_library';
 	org_url: string; // 'https://github.com/fuz-dev';
 	homepage_url: string; // 'https://www.fuz.dev/';
 	homepage_name: string; // 'fuz.dev';
+	published: boolean;
 }
+
+export const parse_package = (url: Url, package_json: PackageJson): Package => {
+	const {name} = package_json;
+
+	// TODO think through with other presentations - Details, Summary, Card
+
+	// TODO hacky
+	const parse_repo = (r: string | null | undefined) => {
+		if (!r) return null;
+		return strip_start(strip_end(r, '.git'), 'git+');
+	};
+
+	const repo = parse_repo(
+		package_json.repository
+			? typeof package_json.repository === 'string'
+				? package_json.repository
+				: package_json.repository.url
+			: null,
+	);
+
+	const format_host = (homepage: string): string => strip_start(new URL(homepage).host, 'www.');
+
+	// TODO for detail view
+	// const license_url = license && repository ? repository + '/blob/main/LICENSE' : null;
+
+	const published =
+		!package_json.private && !!package_json.exports && package_json.version !== '0.0.1';
+
+	const npm_url = published
+		? 'https://www.npmjs.com/package/' + encodeURIComponent(package_json.name)
+		: null;
+
+	const changelog_url = published && repo ? repo + '/blob/main/CHANGELOG.md' : null;
+
+	// TODO proper parsing
+	const repo_name = name[0] === '@' ? name.split('/')[1] : name;
+
+	return {
+		url,
+		package_json,
+		name,
+		npm_url,
+		repo_url,
+		org_url,
+		homepage_url,
+		homepage_name,
+		published,
+	};
+};
