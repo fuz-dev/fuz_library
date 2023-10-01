2840414ed35f3f6d35264cc8f75fda3a4bb4d3b8
Ryan Atkinson <mail@ryanatkn.com>
2023-09-30 22:36:32 -0600

    wip

--- src/lib/PackageSummary.svelte
+++ src/lib/PackageSummary.svelte
@@ -5,15 +5,23 @@
 	export let pkg: PackageMeta; // TODO normalized version with cached primitives?
 
 	$: ({package_json, npm_url, repo_name, repo_url, changelog_url, homepage_url} = pkg);
+	$: ({name, version, description} = package_json);
 </script>
 
 <div class="package_summary">
-	<header class="spaced"><slot {repo_name}><div class="repo_name">{repo_name}</div></slot></header>
-	{#if package_json.description}
-		<blockquote class="spaced">{package_json.description}</blockquote>
+	<!-- TODO maybe continue this slot pattern, or maybe simplify? -->
+	<header class="spaced">
+		<slot name="repo_name" {repo_name}><div class="repo_name">{repo_name}</div></slot>
+	</header>
+	{#if description}
+		<slot name="description" {description}
+			><blockquote class="spaced text_align_center">{description}</blockquote></slot
+		>
 	{/if}
 	{#if npm_url}
-		<a class="npm_url spaced chip box" href={npm_url}>{package_json.name}</a>
+		<slot name="npm_url" {npm_url}
+			><a class="npm_url spaced chip box" href={npm_url}>{name}</a></slot
+		>
 	{/if}
 	{#if homepage_url}
 		<div class="spaced">
@@ -27,7 +35,7 @@
 			<a class="chip" href={repo_url}>repo</a>
 		{/if}
 		{#if changelog_url}
-			<a class="chip" title="version" href={changelog_url}>{package_json.version}</a>
+			<a class="chip" title="version" href={changelog_url}>{version}</a>
 		{/if}
 		<!-- TODO for detail view -->
 		<!-- {#if license_url}
@@ -45,7 +53,6 @@
 		display: flex;
 		flex-direction: column;
 		align-items: center;
-		text-align: center;
 	}
 	.repo_name {
 		/* same as h1 by default (maybe add .h1 utility class?) */
