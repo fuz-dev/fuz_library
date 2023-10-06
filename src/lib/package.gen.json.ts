9eccf039dd50e92842c6930ac6d573de225ffa13
Ryan Atkinson <mail@ryanatkn.com>
2023-10-06 13:10:01 -0600

    wip

--- src/lib/package.gen.json.ts
+++ src/lib/package.gen.json.ts
@@ -1,11 +1,18 @@
-import {format_file} from '@grogarden/gro/format_file.js';
 import type {Gen} from '@grogarden/gro/gen.js';
 import {load_package_json} from '@grogarden/gro/package_json.js';
+import {format_file} from '@grogarden/gro/format_file.js';
 
 // TODO refactor - maybe export this from Gro, or make a configured option
 export const gen: Gen = async () => {
 	return [
-		await format_file('file.json', JSON.stringify(await load_package_json()), {useTabs: false}),
+		{
+			filename: 'package.json',
+			// content: JSON.stringify(await load_package_json(), null, 2),
+			format: false,
+			content: await format_file('', JSON.stringify(await load_package_json()), {
+				useTabs: false,
+			}),
+		},
 		{
 			filename: 'package.json.d.ts',
 			content: `declare module '$lib/package.json' {
